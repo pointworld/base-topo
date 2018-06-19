c48b699418c47c387f0471f034ff426115013735
one <one@point-3.local>
2018-06-19 18:00:08 +0800

    20180619

--- js/main.js
+++ js/main.js
@@ -8,9 +8,9 @@
 require.config({
   urlArgs: "v=1.0.0",
   paths: {
+    'jquery': 'jquery-1.9.1.min',
     'bootstrap': 'bootstrap.min',
     'drag': 'drag',
-    'jquery': 'jquery-1.9.1.min',
     'jtopo': 'jtopo-0.4.8',//核心代码
     'topo-main': 'topo-main',//业务层框架级封装
     'topo-focus': 'topo-focus'//业务层,调用暴露出的接口
